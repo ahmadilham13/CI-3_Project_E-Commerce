935e103ee0e2f3eb85c8677754f4fb77f92b8511
ahmadilham_13 <ahmad13ilham13@gmail.com>
2023-06-28 12:23:24 +0700

    function query in model

--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -33,7 +33,61 @@ class MY_Model extends CI_Model {
         return $this->form_validation->run();
     }
     
+    public function select($columns) {
+        $this->db->select($columns);
+        return $this;
+    }
 
+    public function where($columns, $condition) {
+        $this->db->where($columns, $condition);
+        return $this;
+    }
+
+    public function like($columns, $condition) {
+        $this->db->like($columns, $condition);
+        return $this;
+    }
+
+    public function orLike($columns, $condition) {
+        $this->db->or_like($columns, $condition);
+        return $this;
+    }
+
+    public function join($table, $type = 'left') {
+        $this->db->join($table, "$this->table.id_$table = $table.id", $type);
+        return $this;
+    }
+
+    public function orderBy($columns, $order = 'asc') {
+        $this->db->order_by($columns, $order);
+        return $this;
+    }
+
+    public function first() {
+        return $this->db->get($this->table)->row();
+    }
+
+    public function get() {
+        return $this->db->get($this->table)->result();
+    }
+
+    public function count() {
+        return $this->db->count_all_results($this->table);
+    }
+
+    public function create($data) {
+        $this->db->insert($this->table, $data);
+        return $this->db->insert_id();
+    }
+
+    public function update($data) {
+        return $this->db->update($this->table, $data);
+    }
+
+    public function delete() {
+        $this->db->delete($this->table);
+        return $this->db->affected_rows();
+    }
 }
 
 /* End of file MY_Model.php */
