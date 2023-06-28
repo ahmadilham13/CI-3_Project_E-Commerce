c819becb2678f38d198f0b8f38490ed1905d9783
ahmadilham_13 <ahmad13ilham13@gmail.com>
2023-06-28 13:23:43 +0700

    feat: alert bootstrap

--- application/views/pages/home/index.php
+++ application/views/pages/home/index.php
@@ -1,5 +1,6 @@
   <!-- Content Start -->
   <main class="container">
+    <?php $this->load->view('layouts/_alert'); ?>
     <div class="row">
       <div class="col-md-9">
         <div class="row">
